75949452b3cf5c1d1eb22728d8735150944beb31
jinseoIT <69947833+jinseoIT@users.noreply.github.com>
2021-11-01 15:13:37 +0900

    [merge] api merge

--- src/shared/api/index.js
+++ src/shared/api/index.js
@@ -4,8 +4,7 @@ import { getToken } from '../utils';
 
 /* Axios 인스턴스 설정 */
 const api = axios.create({
-  // baseURL: 'http://localhost:4000',
-  baseURL: 'http://52.79.162.82',
+  baseURL: process.env.REACT_APP_DOMAIN,
 });
 
 /* interceptor를 통한 header 설정 */
