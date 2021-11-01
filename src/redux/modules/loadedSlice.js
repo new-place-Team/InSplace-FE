d6191f3c6acde7a060a899ac31e5d36f67b7f4f8
kokyusik91 <63444730+kokyusik91@users.noreply.github.com>
2021-11-01 09:48:44 +0900

    [feature] : spinner 적용중

--- src/redux/modules/loadedSlice.js
+++ src/redux/modules/loadedSlice.js
@@ -19,3 +19,20 @@ const loadedSlice = createSlice({
 export const { getLoaded } = loadedSlice.actions;
 
 export default loadedSlice;
+
+// const counterSlice = createSlice({
+//   name: 'counter',
+//   initialState: 0,
+//   reducers: {
+//     increment: state => state + 1,
+//     decrement: state => state - 1
+//   }
+// })
+
+// const store = configureStore({
+//   reducer: counterSlice.reducer
+// })
+
+// document.getElementById('increment').addEventListener('click', () => {
+//   store.dispatch(counterSlice.actions.increment())
+// })
