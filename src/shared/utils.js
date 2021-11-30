181da55181338ddddb55cc713826539551a7ae26
kokyusik91 <63444730+kokyusik91@users.noreply.github.com>
2021-11-30 12:58:08 +0900

    [fix] :default 다국어 브라우저에 따라 언어 결정

--- src/shared/utils.js
+++ src/shared/utils.js
@@ -99,3 +99,10 @@ export const setTodayCookie = () => {
   today.setHours(today.getHours() + 24);
   localStorage.setItem('closeBanner', today);
 };
+
+export const getDefaultLanguage = () => {
+  let lang;
+  if (window.navigator.language !== null) {
+    lang = window.navigator.language;
+  }
+};
