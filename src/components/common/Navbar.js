ad21dea566a2a77605584b3f1914458f90d9d2cc
jinseoit <jinseoit@gmail.com>
2021-12-01 13:35:30 +0900

    [fix] 불필요 코드 제거

--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -7,17 +7,10 @@ import { history } from '../../redux/configureStore';
 import { Grid } from '../../elements/index';
 import theme from '../../styles/theme';
 import WeatherDetail from './WeatherDetail';
-/* weather Icon */
-// import { ReactComponent as SunIcon } from '../../images/weather/sun-nav.svg';
-// import { ReactComponent as CloudIcon } from '../../images/weather/cloud.svg';
-// import { ReactComponent as RainIcon } from '../../images/weather/rain.svg';
-// import { ReactComponent as SnowIcon } from '../../images/weather/snow.svg';
 
 /* Nav Icon */
 import { ReactComponent as HomeIcon } from '../../images/nav/ic_nav_home.svg';
-// import { ReactComponent as HomeFillIcon } from '../../images/nav/ic_nav_home-filled.svg';
 import { ReactComponent as NavMap } from '../../images/nav/ic_nav_map.svg';
-// import { ReactComponent as NavMapFill } from '../../images/nav/ic_nav_map-filled.svg';
 import { ReactComponent as FilterIcon } from '../../images/nav/ic_nav_fliter.svg';
 import { ReactComponent as HeartIcon } from '../../images/nav/ic_nav_heart.svg';
 import { ReactComponent as MypageIcon } from '../../images/nav/ic_nav_mypage.svg';
@@ -69,10 +62,6 @@ const Navbar = () => {
       return;
     }
     pageMove(url);
-    // if (!isLoginChk(isLogin)) {
-    //   return;
-    // }
-    // history.push(url);
   };
 
   return (
